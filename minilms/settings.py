5e676a5b8176b41da69ff7ab9f0d14ed57063b72
seokminbaek <gatsby3130@gmail.com>
2025-09-01 11:16:29 +0900

    add redirect

--- minilms/settings.py
+++ minilms/settings.py
@@ -160,3 +160,6 @@ else:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/subjects/'
+LOGOUT_REDIRECT_URL = '/login/'
